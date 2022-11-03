1c60bdeb8f1c497ce01196161b93e2abcbc57501
elina2022 <elinakniazian@gmail.com>
2022-11-03 15:33:11 +0100

    oba 2 en 1 samengevoegd

---
+++ OBA 2/script.js
@@ -0,0 +1,100 @@
+const characters = [
+    {
+        names: 'elina',
+        age: 18,
+        eyeColor: "brown",
+        gender: "female"
+    },
+    {
+        names: 'marina',
+        age: 43,
+        eyeColor: "green",
+        gender: "female"
+    },
+    {
+        names: 'gukas',
+        age: 55,
+        eyeColor: "brown",
+        gender: "male"
+    },
+    {
+        names: 'albert',
+        age: 22,
+        eyeColor: "green",
+        gender: "male"
+    }
+];
+
+const ageFiltered = characters.filter(character => character.age > 25);
+console.log(ageFiltered);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var computer = document.querySelector("#computer")
+// var p1 = document.querySelector("#p-1")
+// var p2 = document.querySelector("#p-2")
+// var p3 = document.querySelector("#p-3")
+// var p4 = document.querySelector("#p-4")
+// var p5 = document.querySelector("#p-5")
+// var p6 = document.querySelector("#p-6")
+// var p7 = document.querySelector("#p-7")
+// var p8 = document.querySelector("#p-8")
+// var p9 = document.querySelector("#p-9")
+// var p10 = document.querySelector("#p-10")
+// var p11 = document.querySelector("#p-11")
+// var p12 = document.querySelector("#p-12")
+// var p13 = document.querySelector("#p-13")
+// var p14 = document.querySelector("#p-14")
+// var p15 = document.querySelector("#p-15")
+// var p16 = document.querySelector("#p-16")
+// var p17 = document.querySelector("#p-17")
+// var p18 = document.querySelector("#p-18")
+// var p19 = document.querySelector("#p-19")
+// var p20 = document.querySelector("#p-20")
+// // var allP = document.querySelector("#allP")
+
+// function showHideComputer(){
+//     p2.style.display = "none"
+//     p3.style.display = "none"
+//     p4.style.display = "none"
+//     p5.style.display = "none"
+//     p6.style.display = "none"
+//     p8.style.display = "none"
+//     p9.style.display = "none"
+//     p11.style.display = "none"
+//     p14.style.display = "none"
+//     p15.style.display = "none"
+//     p16.style.display = "none"
+//     p17.style.display = "none"
+//     p20.style.display = "none"
+// }
