15bc9a8b14516f13a5d33d48d2fb7ecab0559c62
elina2022 <elinakniazian@gmail.com>
2022-12-02 00:42:32 +0100

    ?

---
+++ OBA 2.2 (JS)/script.js
@@ -0,0 +1,183 @@
+// const listsJS = [
+//     {
+//         computer: true,
+//         stopcontact: true,
+//         stilruimte: true,
+//         cafe: false,
+//         wc: true
+//     },
+//     {
+//         computer: false,
+//         stopcontact: true,
+//         stilruimte: true,
+//         cafe: true,
+//         wc: false
+//     },
+//     {
+//         computer: true,
+//         stopcontact: true,
+//         stilruimte: false,
+//         cafe: true,
+//         wc: true
+//     },
+//     {
+//         computer: false,
+//         stopcontact: false,
+//         stilruimte: false,
+//         cafe: false,
+//         wc: false
+//     }
+// ]
+
+// const list = document.querySelectorAll('#list')
+// function showHideList(){
+// }
+
+
+// const computerFiltered = listsJS.filter(list => list.computer == false)
+// console.log(computerFiltered[1])
+
+
+    // array.forEach(item => {
+    //     const li = document.createElement('li')
+    //     li.textContent = item.names
+    //     element.appendChild(li)
+    // })
+
+// const ageFiltered = characters.filter(character => character.age > 21);
+
+// showHideList(ageFiltered, ul)
+
+
+
+
+
+
+
+
+
+
+
+
+// const characters = [
+//     {
+//         names: 'elina ',
+//         age: 18,
+//         eyeColor: ' brown ',
+//         gender: 'female'
+//     },
+//     {
+//         names: 'marina ',
+//         age: 43,
+//         eyeColor: ' green ',
+//         gender: 'female'
+//     },
+//     {
+//         names: 'gukas ',
+//         age: 55,
+//         eyeColor: ' brown ',
+//         gender: 'male'
+//     },
+//     {
+//         names: 'albert ',
+//         age: 22,
+//         eyeColor: ' green ',
+//         gender: 'male'
+//     }
+// ];
+
+// const ul = document.querySelector('#ul')
+// const addP = (array, element) => {
+//     // array.forEach(item => {
+//     //     const li = document.createElement('li')
+//     //     li.textContent = item.names
+//     //     element.appendChild(li)
+//     // })
+//     array.forEach(item => {
+//         const li = document.createElement('li')
+//         li.textContent = item.age
+//         element.appendChild(li)
+//     })
+//     // array.forEach(item => {
+//     //     const li = document.createElement('li')
+//     //     li.textContent = item.eyeColor
+//     //     element.appendChild(li)
+//     // })
+//     // array.forEach(item => {
+//     //     const li = document.createElement('li')
+//     //     li.textContent = item.gender
+//     //     element.appendChild(li)
+//     // })
+// }
+
+// const ageFiltered = characters.filter(character => character.age > 21);
+// // const genderFiltered = characters.filter(character => character.gender === 'female')
+
+// addP(ageFiltered, ul)
+// // addP(genderFiltered, li)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var computer = document.querySelector("#computer")
+// var p1 = document.querySelector("#p-1")
+// var p2 = document.querySelector("#p-2")
+// var p3 = document.querySelector("#p-3")
+// var p4 = document.querySelector("#p-4")
+// var p5 = document.querySelector("#p-5")
+// var p6 = document.querySelector("#p-6")
+// var p7 = document.querySelector("#p-7")
+// var p8 = document.querySelector("#p-8")
+// var p9 = document.querySelector("#p-9")
+// var p10 = document.querySelector("#p-10")
+// var p11 = document.querySelector("#p-11")
+// var p12 = document.querySelector("#p-12")
+// var p13 = document.querySelector("#p-13")
+// var p14 = document.querySelector("#p-14")
+// var p15 = document.querySelector("#p-15")
+// var p16 = document.querySelector("#p-16")
+// var p17 = document.querySelector("#p-17")
+// var p18 = document.querySelector("#p-18")
+// var p19 = document.querySelector("#p-19")
+// var p20 = document.querySelector("#p-20")
+// // var allP = document.querySelector("#allP")
+
+// function showHideComputer(){
+//     p2.style.display = "none"
+//     p3.style.display = "none"
+//     p4.style.display = "none"
+//     p5.style.display = "none"
+//     p6.style.display = "none"
+//     p8.style.display = "none"
+//     p9.style.display = "none"
+//     p11.style.display = "none"
+//     p14.style.display = "none"
+//     p15.style.display = "none"
+//     p16.style.display = "none"
+//     p17.style.display = "none"
+//     p20.style.display = "none"
+// }
