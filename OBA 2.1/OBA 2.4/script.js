753192d34a1918f5b8045b90f8d8dc2971853c70
elina2022 <elinakniazian@gmail.com>
2022-12-09 02:25:31 +0100

    d

---
+++ OBA 2.1/OBA 2.4/script.js
@@ -0,0 +1,64 @@
+const $ = element => document.querySelector(element)
+const $all = element => document.querySelectorAll(element)
+
+let btn = $(".boekButton")
+let begin = $("#containerBegintijd")
+let eind = $("#containerEindtijd")
+let arrow = $(".more2")
+let btnText = $(".reserveer")
+let tijden = $all(".timeAvailable")
+let tijden2 = $all(".timeAvailable2")
+let tijdenAlle = $all(".time")
+
+btn.addEventListener("click", showTijd)
+for (tijd of tijden) {tijd.addEventListener('click', buttonComesDown)}
+for (tijd2 of tijden2) {tijd2.addEventListener('click', buttonComesDown2)}
+
+function showTijd(){
+    if(btnText.innerHTML == "Ga verder"){
+        eind.style.height = "6rem"
+        arrow.style.rotate = "90deg"
+        eind.style.marginBottom = "1rem"
+    } else {
+        begin.style.height = "5rem"
+        arrow.style.rotate = "90deg"
+    }
+}
+
+function buttonComesDown(){
+    btnText.innerHTML = "Ga verder"
+    arrow.style.rotate = "0deg"
+    btn.style.top = "4.2rem"
+    btn.style.backgroundColor = "rgb(245, 245, 245)"
+    btn.style.boxShadow = "inset 0px 0px 4px 0px black"
+    // btn.style.border = "1px solid black"
+    btnText.style.color = "black"
+    arrow.style.fill = "black"
+}
+
+function buttonComesDown2(){
+    arrow.style.rotate = "0deg"
+    btnText.innerHTML = "Reserveer!"
+    btn.style.top = "10.75rem"
+}
+
+function tijdStyle(){
+    tijdenAlle.style.border = "solid 1px black"
+}
+
+function done(){
+    if(btnText.innerHTML == "Reserveer!"){
+    } 
+}
+
+// let logo = document.querySelector("#logo");
+// logo = true
+// function toggle(){
+//     if(logo === true){
+//     logo.style.width = "1000px"
+//     logo = false
+// } else {
+//     logo.style.width = "10px"
+//     logo = true
+// }
+// }
